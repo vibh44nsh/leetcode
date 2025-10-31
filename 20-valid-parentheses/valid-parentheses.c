9f54cb7b0c8c041bd7982e00293bdbe1d16af583
vibh44nsh <vibh44nshb@gmail.com>
2025-10-31 15:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,19 @@
+bool isValid(char * s) {
+    char stack[strlen(s)];
+    int top = -1;
+    
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack[++top] = s[i];
+        } else {
+            if (top == -1 || 
+               (s[i] == ')' && stack[top] != '(') || 
+               (s[i] == '}' && stack[top] != '{') || 
+               (s[i] == ']' && stack[top] != '[')) {
+                return false;
+            }
+            top--;
+        }
+    }
+    return top == -1;
+}
