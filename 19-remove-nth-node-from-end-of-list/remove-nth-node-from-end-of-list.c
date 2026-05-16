8093e4c3f372ff4513ac4caa8f9f15825e3b2bfd
vibh44nsh <181543252+vibh44nsh@users.noreply.github.com>
2026-05-16 17:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (80.05%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.c
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.c
@@ -6,29 +6,21 @@
  * };
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
-    dummy->val = 0;
+    struct ListNode* dummy = (struct ListNode* ) malloc(sizeof(struct ListNode));
     dummy->next = head;
-    
     struct ListNode* fast = dummy;
     struct ListNode* slow = dummy;
-
-    for (int i = 0; i <= n; i++) {
+    for(int i = 0; i <= n; i++){
         fast = fast->next;
     }
-
-    while (fast != NULL) {
+    while(fast){
         fast = fast->next;
         slow = slow->next;
     }
+    struct ListNode* del = slow->next;
+    slow->next = del->next;
+    del->next = NULL;
+    free(del);
 
-    struct ListNode* temp = slow->next;
-    slow->next = slow->next->next;
-    
-    free(temp);
-    
-    struct ListNode* new_head = dummy->next;
-    free(dummy);
-    
-    return new_head;
+    return dummy->next;
 }
