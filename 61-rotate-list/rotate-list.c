0223fe4a3ea252c49b365a4642fec9e2d6bb2668
vibh44nsh <181543252+vibh44nsh@users.noreply.github.com>
2026-05-17 16:09:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (71.57%) - LeetSync

--- 61-rotate-list/rotate-list.c
+++ 61-rotate-list/rotate-list.c
@@ -6,26 +6,23 @@
  * };
  */
 struct ListNode* rotateRight(struct ListNode* head, int k) {
-    if (head == NULL || head->next == NULL || k == 0) {
-        return head;
-    }
-
+    if(head == NULL || head->next == NULL || k == 0) return head;
     int length = 1;
-    struct ListNode* current = head;
-    while (current->next != NULL) {
-        current = current->next;
+    struct ListNode* tail = head;
+    while(tail->next){
+        tail = tail->next;
         length++;
     }
-    current->next = head;
-    k = k % length;
-
-    struct ListNode* newTail = head;
-    for (int i = 0; i < length - k - 1; i++) {
-        newTail = newTail->next;
+    k %= length;
+    if(k == 0){
+        return head;
     }
-
-    struct ListNode* newHead = newTail->next;
-    newTail->next = NULL;
-
-    return newHead;
+    tail->next = head;
+    struct ListNode* tail_new = head;
+    for(int i = 0; i < length - k - 1; i++){
+        tail_new = tail_new->next;
+    }
+    struct ListNode* head_new = tail_new->next;
+    tail_new->next = NULL;
+    return head_new;
 }
